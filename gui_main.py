a9ab8f1a9eae8846fbbb862a5e9103f69f9502cb
neurobrko <elvis@elvis.sk>
2024-04-17 00:47:28 +0200

    executables, day 18

--- gui_main.py
+++ gui_main.py
@@ -1,6 +1,11 @@
 import functions
 import PySimpleGUI as sg
 import time
+import os
+
+if not os.path.exists("todos.txt"):
+    with open("todos.txt", "w") as file:
+        pass
 
 sg.theme("DarkGrey6")
 
@@ -10,24 +15,12 @@ popup_title = "Warning"
 clock = sg.Text("", key="clock")
 label = sg.Text("Type in a to-do:")
 input_box = sg.InputText(tooltip="Enter todo...", key="todo")
-add_button = sg.Button(
-    size=2,
-    image_source="button_images/add.png",
-    mouseover_colors="LightBlue2",
-    tooltip="Add a Todo",
-    key="Add",
-)
+add_button = sg.Button("Add", key="Add")
 list_box = sg.Listbox(
     values=functions.get_todos(), key="todos", enable_events=True, size=(45, 10)
 )
 edit_button = sg.Button("Edit")
-complete_button = sg.Button(
-    size=2,
-    image_source="button_images/complete.png",
-    mouseover_colors="LightBlue2",
-    tooltip="Complete a Todo",
-    key="Complete",
-)
+complete_button = sg.Button("Complete", key="Complete")
 exit_button = sg.Button("Exit")
 
 window = sg.Window(
