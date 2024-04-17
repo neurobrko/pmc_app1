1c2074fe7645f14cdcdc127b664dbe6dab68b95d
neurobrko <elvis@elvis.sk>
2024-04-17 22:57:14 +0200

    dynamic webapp, day 19

--- web_main.py
+++ web_main.py
@@ -1,12 +1,38 @@
 import streamlit as st
 import functions
 
+todos = functions.get_todos()
+
+
+def add_todo():
+    todo = st.session_state["new_todo"] + "\n"
+    st.session_state.new_todo = ""
+    todos.append(todo)
+    functions.write_todos(todos)
+
+
+def complete_todo():
+    pass
+
+
 st.title("My Todo App")
 st.subheader("This is my todo app.")
 st.write("This app is to increase your productivity.")
 
-todos = functions.get_todos()
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, on_change=complete_todo, key=index)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[index]
+        st.rerun()
 
-st.text_input("", placeholder="Add new todo...")
+st.text_input(
+    label="New Todo",
+    label_visibility="hidden",
+    placeholder="Add new todo...",
+    on_change=add_todo,
+    key="new_todo",
+)
+
+st.session_state
